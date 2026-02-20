efe29d707b25bab7a9747d62a6144c5f8cfeab76
Rajputharsh25 <144730809+Rajputharsh25@users.noreply.github.com>
2026-02-20 19:08:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,53 +1,37 @@
-// Rectangle Class
 class Rectangle {
-    constructor(width, height) {
-        this._width = width;
-        this._height = height;
-    }
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-    // Getter for width
-    get width() {
-        return this._width;
-    }
+  // Getter for width
+  get width() {
+    return this._width;
+  }
 
-    // Getter for height
-    get height() {
-        return this._height;
-    }
+  // Getter for height
+  get height() {
+    return this._height;
+  }
 
-    // Method to calculate area
-    getArea() {
-        return this._width * this._height;
-    }
+  // Method to calculate area
+  getArea() {
+    return this.width * this.height;
+  }
 }
 
-
-// Square Class (inherits from Rectangle)
 class Square extends Rectangle {
-    constructor(side) {
-        super(side, side); // sets width and height equal
-    }
+  constructor(side) {
+    // A square is a rectangle where width = height = side
+    super(side, side);
+  }
 
-    // Method to calculate perimeter
-    getPerimeter() {
-        return this.width * 4;
-    }
+  // Method to calculate perimeter
+  getPerimeter() {
+    return 4 * this.width;
+  }
 }
 
-
-// Make classes available globally for Cypress tests
+// Attach to window for Cypress tests to access
 window.Rectangle = Rectangle;
 window.Square = Square;
-
-
-// Optional test examples
-const rectangle = new Rectangle(5, 10);
-console.log(rectangle.width);      // 5
-console.log(rectangle.height);     // 10
-console.log(rectangle.getArea());  // 50
-
-const square = new Square(7);
-console.log(square.width);         // 7
-console.log(square.height);        // 7
-console.log(square.getArea());     // 49
-console.log(square.getPerimeter()); // 28
